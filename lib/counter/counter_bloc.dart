eb352b045c5404140f4b0dfbee57ca323df440b6
Sonu Raj <sonuraj909@gmail.com>
2022-10-27 17:17:12 +0530

    inital commit

---
+++ lib/counter/counter_bloc.dart
@@ -0,0 +1,18 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'counter_event.dart';
+part 'counter_state.dart';
+part 'counter_bloc.freezed.dart';
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(CounterState.initial()) {
+    on<Increment>((event, emit) {
+      return emit(state.copyWith(count: state.count + 1));
+    });
+    on<Decrement>((event, emit) {
+      return emit(state.copyWith(count: state.count - 1));
+    });
+  }
+}
---
+++ lib/counter/counter_bloc.freezed.dart
@@ -0,0 +1,415 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'counter_bloc.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$CounterEvent {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() increment,
+    required TResult Function() decrement,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? increment,
+    TResult Function()? decrement,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? increment,
+    TResult Function()? decrement,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(Increment value) increment,
+    required TResult Function(Decrement value) decrement,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(Increment value)? increment,
+    TResult Function(Decrement value)? decrement,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(Increment value)? increment,
+    TResult Function(Decrement value)? decrement,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $CounterEventCopyWith<$Res> {
+  factory $CounterEventCopyWith(
+          CounterEvent value, $Res Function(CounterEvent) then) =
+      _$CounterEventCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class _$CounterEventCopyWithImpl<$Res> implements $CounterEventCopyWith<$Res> {
+  _$CounterEventCopyWithImpl(this._value, this._then);
+
+  final CounterEvent _value;
+  // ignore: unused_field
+  final $Res Function(CounterEvent) _then;
+}
+
+/// @nodoc
+abstract class _$$IncrementCopyWith<$Res> {
+  factory _$$IncrementCopyWith(
+          _$Increment value, $Res Function(_$Increment) then) =
+      __$$IncrementCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$IncrementCopyWithImpl<$Res> extends _$CounterEventCopyWithImpl<$Res>
+    implements _$$IncrementCopyWith<$Res> {
+  __$$IncrementCopyWithImpl(
+      _$Increment _value, $Res Function(_$Increment) _then)
+      : super(_value, (v) => _then(v as _$Increment));
+
+  @override
+  _$Increment get _value => super._value as _$Increment;
+}
+
+/// @nodoc
+
+class _$Increment with DiagnosticableTreeMixin implements Increment {
+  const _$Increment();
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'CounterEvent.increment()';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty('type', 'CounterEvent.increment'));
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$Increment);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() increment,
+    required TResult Function() decrement,
+  }) {
+    return increment();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? increment,
+    TResult Function()? decrement,
+  }) {
+    return increment?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? increment,
+    TResult Function()? decrement,
+    required TResult orElse(),
+  }) {
+    if (increment != null) {
+      return increment();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(Increment value) increment,
+    required TResult Function(Decrement value) decrement,
+  }) {
+    return increment(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(Increment value)? increment,
+    TResult Function(Decrement value)? decrement,
+  }) {
+    return increment?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(Increment value)? increment,
+    TResult Function(Decrement value)? decrement,
+    required TResult orElse(),
+  }) {
+    if (increment != null) {
+      return increment(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class Increment implements CounterEvent {
+  const factory Increment() = _$Increment;
+}
+
+/// @nodoc
+abstract class _$$DecrementCopyWith<$Res> {
+  factory _$$DecrementCopyWith(
+          _$Decrement value, $Res Function(_$Decrement) then) =
+      __$$DecrementCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$DecrementCopyWithImpl<$Res> extends _$CounterEventCopyWithImpl<$Res>
+    implements _$$DecrementCopyWith<$Res> {
+  __$$DecrementCopyWithImpl(
+      _$Decrement _value, $Res Function(_$Decrement) _then)
+      : super(_value, (v) => _then(v as _$Decrement));
+
+  @override
+  _$Decrement get _value => super._value as _$Decrement;
+}
+
+/// @nodoc
+
+class _$Decrement with DiagnosticableTreeMixin implements Decrement {
+  const _$Decrement();
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'CounterEvent.decrement()';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty('type', 'CounterEvent.decrement'));
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$Decrement);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() increment,
+    required TResult Function() decrement,
+  }) {
+    return decrement();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? increment,
+    TResult Function()? decrement,
+  }) {
+    return decrement?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? increment,
+    TResult Function()? decrement,
+    required TResult orElse(),
+  }) {
+    if (decrement != null) {
+      return decrement();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(Increment value) increment,
+    required TResult Function(Decrement value) decrement,
+  }) {
+    return decrement(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(Increment value)? increment,
+    TResult Function(Decrement value)? decrement,
+  }) {
+    return decrement?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(Increment value)? increment,
+    TResult Function(Decrement value)? decrement,
+    required TResult orElse(),
+  }) {
+    if (decrement != null) {
+      return decrement(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class Decrement implements CounterEvent {
+  const factory Decrement() = _$Decrement;
+}
+
+/// @nodoc
+mixin _$CounterState {
+  int get count => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $CounterStateCopyWith<CounterState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $CounterStateCopyWith<$Res> {
+  factory $CounterStateCopyWith(
+          CounterState value, $Res Function(CounterState) then) =
+      _$CounterStateCopyWithImpl<$Res>;
+  $Res call({int count});
+}
+
+/// @nodoc
+class _$CounterStateCopyWithImpl<$Res> implements $CounterStateCopyWith<$Res> {
+  _$CounterStateCopyWithImpl(this._value, this._then);
+
+  final CounterState _value;
+  // ignore: unused_field
+  final $Res Function(CounterState) _then;
+
+  @override
+  $Res call({
+    Object? count = freezed,
+  }) {
+    return _then(_value.copyWith(
+      count: count == freezed
+          ? _value.count
+          : count // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$$_CounterStateCopyWith<$Res>
+    implements $CounterStateCopyWith<$Res> {
+  factory _$$_CounterStateCopyWith(
+          _$_CounterState value, $Res Function(_$_CounterState) then) =
+      __$$_CounterStateCopyWithImpl<$Res>;
+  @override
+  $Res call({int count});
+}
+
+/// @nodoc
+class __$$_CounterStateCopyWithImpl<$Res>
+    extends _$CounterStateCopyWithImpl<$Res>
+    implements _$$_CounterStateCopyWith<$Res> {
+  __$$_CounterStateCopyWithImpl(
+      _$_CounterState _value, $Res Function(_$_CounterState) _then)
+      : super(_value, (v) => _then(v as _$_CounterState));
+
+  @override
+  _$_CounterState get _value => super._value as _$_CounterState;
+
+  @override
+  $Res call({
+    Object? count = freezed,
+  }) {
+    return _then(_$_CounterState(
+      count: count == freezed
+          ? _value.count
+          : count // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_CounterState with DiagnosticableTreeMixin implements _CounterState {
+  const _$_CounterState({required this.count});
+
+  @override
+  final int count;
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'CounterState(count: $count)';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('type', 'CounterState'))
+      ..add(DiagnosticsProperty('count', count));
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_CounterState &&
+            const DeepCollectionEquality().equals(other.count, count));
+  }
+
+  @override
+  int get hashCode =>
+      Object.hash(runtimeType, const DeepCollectionEquality().hash(count));
+
+  @JsonKey(ignore: true)
+  @override
+  _$$_CounterStateCopyWith<_$_CounterState> get copyWith =>
+      __$$_CounterStateCopyWithImpl<_$_CounterState>(this, _$identity);
+}
+
+abstract class _CounterState implements CounterState {
+  const factory _CounterState({required final int count}) = _$_CounterState;
+
+  @override
+  int get count;
+  @override
+  @JsonKey(ignore: true)
+  _$$_CounterStateCopyWith<_$_CounterState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
